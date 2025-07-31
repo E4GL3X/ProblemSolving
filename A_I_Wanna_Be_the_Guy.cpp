2f7c464797b8c9fea5eb4bd47c6c5ae7017ea4ba
e4gl3x <mohammadtohasiddique@gmail.com>
2025-07-31 23:33:17 +0600

    Commit

---
+++ A_I_Wanna_Be_the_Guy.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    set<int> levels;
+    
+    int p;
+    cin >> p;
+    for (int i = 0; i < p; i++) {
+        int x;
+        cin >> x;
+        levels.insert(x);
+    }
+    
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        int y;
+        cin >> y;
+        levels.insert(y);
+    }
+    
+    cout << (levels.size() == n ? "I become the guy." : "Oh, my keyboard!") << endl;
+    return 0;
+}
