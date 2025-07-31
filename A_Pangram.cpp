7034bf0cee672c913beccab4088153091f0ffb49
Toha Siddique <mohammadtohasiddique@gmail.com>
2025-08-01 00:05:55 +0600

    Git

---
+++ A_Pangram.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+string pangram(int n, const string s){
+    set<char> letters;
+    for (int i = 0; i < n; i++) {
+        if (isalpha(s[i])) {
+            letters.insert(tolower(s[i]));
+        }
+    }
+    return letters.size() == 26 ? "YES" : "NO";
+}
+ 
+int main()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    cout << pangram(n, s) << endl;
+    return 0;
+}
