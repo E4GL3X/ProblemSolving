cfe0c482c3752d683ac1b479ed0114fc942979e9
Toha Siddique <mohammadtohasiddique@gmail.com>
2025-08-31 22:59:02 +0600

    Push

--- A_Hit_the_Lottery.cpp
+++ A_Hit_the_Lottery.cpp
@@ -2,14 +2,13 @@
 using namespace std;
 int main()
 {
-    int n, minimum_number_of_bills = 0;
-    int array[] = {100, 20, 10, 5, 1};
+    int n, min = 0;
+    int arr[] = {100, 20, 10, 5, 1};
     cin >> n;
-    int i;
-    for (i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++)
     {
-        minimum_number_of_bills += n / array[i];
-        n = n % array[i];
+        min += n / arr[i];
+        n = n % arr[i];
     }
-    cout << minimum_number_of_bills << endl;
+    cout << min << endl;
 }
